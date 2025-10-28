7f46cb49a98a4ddb1b2ca41239c4db00f0032607
Daniel Pinna <daniel@pinna.dev.br>
2025-10-28 00:46:31 -0300

    refactor: remove 'mocks' and use repositories to get informations

--- src/main/java/br/dev/pinna/controller/ExchangeController.java
+++ src/main/java/br/dev/pinna/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package br.dev.pinna.controller;
 
 import br.dev.pinna.environment.InstanceInformationService;
 import br.dev.pinna.model.Exchange;
+import br.dev.pinna.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,20 @@ public class ExchangeController {
     @Autowired
     InstanceInformationService informationService;
 
+    @Autowired
+    ExchangeRepository repository;
+
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
+        Exchange exchange = repository.findByFromAndTo(from, to);
+
+        if(exchange == null) throw new RuntimeException("Currency Unsupported!");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment("PORT " + informationService.retrieveServerPort());
+
+        return exchange;
     }
 }
