0163d807186d408b8aed82ec51e57679646d35de
Daniel Pinna <daniel@pinna.dev.br>
2025-10-27 23:46:29 -0300

    refactor: implements service to get port on server dinamically

--- src/main/java/br/dev/pinna/controller/ExchangeController.java
+++ src/main/java/br/dev/pinna/controller/ExchangeController.java
@@ -1,6 +1,8 @@
 package br.dev.pinna.controller;
 
+import br.dev.pinna.environment.InstanceInformationService;
 import br.dev.pinna.model.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +15,11 @@ import java.math.BigDecimal;
 @RequestMapping("exchange-service")
 public class ExchangeController {
 
+    @Autowired
+    InstanceInformationService informationService;
+
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
     }
 }
